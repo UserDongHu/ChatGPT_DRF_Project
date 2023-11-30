505ca8d49ea088a3303c3ac437e40b9f15db5389
KimDongHu <ehdgn1022@naver.com>
2023-11-30 17:44:30 +0900

    DRF Project blog앱 POST 권한 오류 수정

--- blog/permissions.py
+++ blog/permissions.py
@@ -4,4 +4,7 @@ class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
+        if request.method in ['POST', 'GET']:
+            return True
+        
         return obj.user == request.user
